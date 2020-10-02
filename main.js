c46c82e3cf24d82b73e57a276977408ac747a391
Mohamed Bayrem YAHYAOUI <mb.yahyaoui@SCANTECH.local>
2020-10-02 17:03:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+//var product = 'Socks';
+var app = new Vue({
+	el: '#app',
+	data: {
+		product: 'Socks',
+		image: './greenSocks.jpg',
+		inStock: true,
+		inventory: 100
+	}
+})
