cf6882898edd91fea90d846988b55da1b93b9616
yahyaoui-mohamed-bayrem <y.bayrem@gmail.com>
2020-10-02 20:40:43 +0100

    5-EventHandling

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ var app = new Vue({
 		product: 'Socks',
 		image: './greenSocks.jpg',
 		inStock: true,
-		inventory: 100
+		inventory: 100,
+		details: ["80% coton", "20% polyester", "Gender-neutral"],
+		variants: [
+			{
+				variantID: 2234,
+				variantColor:"green",
+				variantImage:"./greenSocks.jpg"
+			},
+			{
+				variantID: 2235,
+				variantColor:"blue",
+				variantImage:"./blueSocks.jpg"
+			}
+		],
+		cart: 0
+	},
+	methods: {
+		addToCart: function () {
+			this.cart += 1;
+		},
+		updateProduct: function (variantImage) {
+			this.image = variantImage;
+		}
 	}
 })
