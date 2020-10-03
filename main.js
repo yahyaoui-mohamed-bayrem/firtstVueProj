cee12ea6dd1e5ceb5f0b5ee11f69828c0d223c8e
Mohamed Bayrem YAHYAOUI <mb.yahyaoui@SCANTECH.local>
2020-10-03 12:21:41 +0100

    7-computed properties

--- main.js
+++ main.js
@@ -3,21 +3,25 @@
 var app = new Vue({
 	el: '#app',
 	data: {
+		brand: 'Vue Mastery',
 		product: 'Socks',
-		image: './greenSocks.jpg',
-		inStock: true,
+		/* image: './greenSocks.jpg',*/
+		selectedVariant: 0,
+		//inStock: true,
 		inventory: 100,
 		details: ["80% coton", "20% polyester", "Gender-neutral"],
 		variants: [
 			{
 				variantID: 2234,
 				variantColor:"green",
-				variantImage:"./greenSocks.jpg"
+				variantImage:"./greenSocks.jpg",
+				variantQantity: 0
 			},
 			{
 				variantID: 2235,
 				variantColor:"blue",
-				variantImage:"./blueSocks.jpg"
+				variantImage:"./blueSocks.jpg",
+				variantQantity: 15
 			}
 		],
 		cart: 0
@@ -26,8 +30,19 @@ var app = new Vue({
 		addToCart: function () {
 			this.cart += 1;
 		},
-		updateProduct: function (variantImage) {
-			this.image = variantImage;
+		updateProduct: function (index) {
+			this.selectedVariant = index;
+		}
+	},
+	computed: {
+		title(){
+			return this.brand + ' ' + this.product
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQantity
 		}
 	}
 })
