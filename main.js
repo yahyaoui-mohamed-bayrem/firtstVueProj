417834691d2d543c0e40e091f2f5e55deed0cc1a
yahyaoui-mohamed-bayrem <y.bayrem@gmail.com>
2020-10-05 21:58:23 +0100

    9-communicatingEvents

--- main.js
+++ main.js
@@ -1,14 +1,67 @@
 // JavaScript source code
 //var product = 'Socks';
-var app = new Vue({
-	el: '#app',
-	data: {
-		brand: 'Vue Mastery',
+Vue.component('product', {
+	props: {
+		premium: {
+			type: Boolean,
+			required: true
+		}
+	},
+	template: `
+	<div class="product"> 
+            <div class="product-image">
+                <!-- vue's v-bind directive create a bind between data & the attribute we wana be bound to -->
+                <img v-bind:src="image" width="300px">
+            </div>
+            <div class="product-info">
+                <h2>{{ title }}</h2>
+                <!--
+                try v-show -- it adds a display-none in the css of the element -- it doesn't change the dom -- faster
+                <p v-if="inStock">In stock</p>
+                <p v-else>Out of stock</p>
+                -->
+                <p v-if="inventory > 10">In stock</p>
+                <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+                <p v-else>Out of stock</p>
+				<p>Shipping: {{ shipping }}</p>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+
+                <!-- 
+                <div v-for="variant in variants" 
+                    :key="variant.variantID"
+                    class="color-box"
+                    :style="{ backgroundColor: variant.variantColor }"
+                    @mouseover="updateProduct( variant.variantImage )">
+                -->
+                <!-- Same thing but with a computed prop image()-->
+                <div v-for="(variant, index) in variants" 
+                    :key="variant.variantID"
+                    class="color-box"
+                    :style="{ backgroundColor: variant.variantColor }"
+                    @mouseover="updateProduct( index )">
+                    <!-- @ is a chorthand for v-on 
+                    <p @mouseover="updateProduct( variant.variantImage )">{{ variant.variantColor }}</p>
+                    -->
+                </div>
+
+                <button v-on:click="addToCart"
+                        :disabled="!inStock"
+                        :class="{ disabledButton: !inStock }">Add to Cart</button>
+
+                
+            </div>
+        </div>
+	`,
+	data(){
+		return {
+			brand: 'Vue Mastery',
 		product: 'Socks',
 		/* image: './greenSocks.jpg',*/
 		selectedVariant: 0,
 		//inStock: true,
-		inventory: 100,
+		//inventory: 100,
 		details: ["80% coton", "20% polyester", "Gender-neutral"],
 		variants: [
 			{
@@ -23,12 +76,14 @@ var app = new Vue({
 				variantImage:"./blueSocks.jpg",
 				variantQantity: 15
 			}
-		],
-		cart: 0
+		]
+		}
 	},
 	methods: {
 		addToCart: function () {
-			this.cart += 1;
+			this.$emit('add-to-cart', this.variants[this.selectedVariant].variantID)
+			//emitting an event by calling it add-to-cart, so somethig has to receave this call 
+			//--> we will listen for that call in the product component by @add-to-acrt
 		},
 		updateProduct: function (index) {
 			this.selectedVariant = index;
@@ -43,6 +98,29 @@ var app = new Vue({
 		},
 		inStock() {
 			return this.variants[this.selectedVariant].variantQantity
+		},
+		inventory(){
+			return this.selectedVariant ? 
+						this.variants[this.selectedVariant].variantQantity :
+						this.variants[0].variantQantity;
+		},
+		shipping() {
+			if (this.premium) {
+				return 'Free'
+			} 
+			return 2.99
 		}
 	}
+})
+var app = new Vue({
+	el: '#app',
+	data: {
+		premium: true,
+		cart: []
+	},
+	methods:{
+		updateCart (id){
+			this.cart.push(id)
+		}
+	}	
 })
